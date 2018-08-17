b64b6da83d07d0709e246cdcd5a85e1d6edb02b0
tc9011 <tc9011@126.com>
2018-08-18 00:07:14 +0800

    feat(app): add shared module

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+const SHAREDMODULE = [
+  ReactiveFormsModule,
+  FormsModule,
+  RouterModule
+];
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [],
-  exports: []
+  declarations: [...SHAREDMODULE],
+  exports: [...SHAREDMODULE]
 })
 export class SharedModule { }
