8abf43c922acaacc54b8dc5cd61ef3dab6f4d16c
tc9011 <tc9011@126.com>
2018-08-19 23:41:32 +0800

    feat(server): add new dir

--- server/app.ts
+++ server/app.ts
@@ -2,9 +2,14 @@ import * as Koa from 'koa';
 import * as mongoose from 'mongoose';
 
 const app = new Koa();
+const dbURL = 'mongodb://localhost/gradingSystem';
+
+mongoose.connect(dbURL);
 
 app.use(ctx => {
   ctx.body = 'Hello Koa';
 });
 
 app.listen(3000);
+
+export { app };
