35c690666b4df140eef419528c00f28f6187d44d
tc9011 <tc9011@126.com>
2018-08-26 00:17:13 +0800

    refactor(server): change handle type

--- server/utils/handle.ts
+++ server/utils/handle.ts
@@ -4,17 +4,17 @@ export interface HandleParams {
   ctx: Context;
   message: string;
   err?: any;
-  result?: any;
+  response?: any;
 }
 
 export const handleError = (
   { ctx, message = '请求失败', err = '' }: HandleParams
 ) => {
-  ctx.body = { code: 0, message, debug: err };
+  ctx.body = { status: 0, message, debug: err };
 };
 
 export const handleSuccess = (
-  { ctx, message = '请求成功', result = '' }: HandleParams
+  { ctx, message = '请求成功', response = '' }: HandleParams
 ) => {
-  ctx.response.body = { code: 1, message, result };
+  ctx.response.body = { status: 1, message, response };
 };
