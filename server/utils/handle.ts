e40911674bdf167458f4eee5f251ab9162bb792e
10227461 <tang.cheng11@zte.com.cn>
2018-08-28 16:21:27 +0800

    refactor(server): change status

--- server/utils/handle.ts
+++ server/utils/handle.ts
@@ -10,11 +10,11 @@ export interface HandleParams {
 export const handleError = (
   { ctx, message = '请求失败', err = '' }: HandleParams
 ) => {
-  ctx.body = { status: 0, message, debug: err };
+  ctx.body = { status: 1, message, debug: err };
 };
 
 export const handleSuccess = (
   { ctx, message = '请求成功', response = '' }: HandleParams
 ) => {
-  ctx.response.body = { status: 1, message, response };
+  ctx.response.body = { status: 0, message, response };
 };
