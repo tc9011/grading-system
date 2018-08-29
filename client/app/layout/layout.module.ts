0a33b8423f0fa33b6a8cfbd2cb5fb09b174fd444
10227461 <tang.cheng11@zte.com.cn>
2018-08-29 17:23:44 +0800

    feat(page-header): add page-header

--- client/app/layout/layout.module.ts
+++ client/app/layout/layout.module.ts
@@ -7,12 +7,14 @@ import { SharedModule } from '../shared/shared.module';
 import { GlobalComponent } from './global/global.component';
 import { PassportComponent } from './passport/passport.component';
 import { UserComponent } from './header/component/user/user.component';
+import { PageHeaderComponent } from './page-header/page-header.component';
 
 const LAYOUTCOMPONENT = [
   HeaderComponent,
   SidebarComponent,
   GlobalComponent,
-  PassportComponent
+  PassportComponent,
+  PageHeaderComponent
 ];
 
 @NgModule({
