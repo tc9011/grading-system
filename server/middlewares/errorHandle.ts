3de1d111def379a7109bf342e29dbcd5ed6fbe37
tc9011 <tc9011@126.com>
2018-08-30 23:42:59 +0800

    refactor(server): add error message

--- server/middlewares/errorHandle.ts
+++ server/middlewares/errorHandle.ts
@@ -1,11 +1,11 @@
 // Custom 401 handling if you don't want to expose koa-jwt errors to users
+import { handleError } from '../utils/handle';
+
 export const errorHandle = (ctx, next) => {
   return next().catch((err) => {
     if (err.status === 401) {
       ctx.status = 401;
-      ctx.body = {
-        error: err.originalError ? err.originalError.message : err.message,
-      };
+      handleError({ctx, message: '登录过期，请重新登录', err: err.originalError ? err.originalError.message : err.message});
     } else {
       throw err;
     }
