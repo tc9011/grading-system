144edbaaa15e639a8aadbaaa3ce7a8ac9ecd2cbe
10227461 <tang.cheng11@zte.com.cn>
2018-08-30 17:29:30 +0800

    refactor(server): add selfEvaluation type

--- server/app/models/selfEvaluation.ts
+++ server/app/models/selfEvaluation.ts
@@ -1,82 +1,56 @@
-import { Document, Schema } from 'mongoose';
 import * as bcrypt from 'bcryptjs';
+import { pre, prop, Typegoose, instanceMethod } from 'typegoose';
 
 const SALT_WORK_FACTOR = 10;
 
-export interface SelfEvaluation extends Document {
-  workNumber: string;
-  data: Date;
-  achievement: string;
-  share: string;
-  contribution: string;
-  meta: any;
-}
-
-const SelfEvaluationSchema: Schema = new Schema({
-  workNumber: {
-    unique: true,
-    type: String,
-  },
-
-  data: Date,
-
-  achievement: String,
-
-  share: String,
-
-  contribution: String,
-
-  meta: {
-    createAt: {
-      type: Date,
-      default: Date.now(),
-    },
-    updateAt: {
-      type: Date,
-      default: Date.now(),
-    },
-  },
-});
-
-SelfEvaluationSchema.pre('save', async function (next) {
-  const selfEvaluation: SelfEvaluation = <SelfEvaluation>this;
-
+@pre<SelfEvaluationSchema>('save', async function (next) {
   if (this.isNew) {
-    selfEvaluation.meta.createAt = selfEvaluation.meta.updateAt = Date.now();
+    this.createAt = this.updateAt = Date.now();
   } else {
-    selfEvaluation.meta.updateAt = Date.now();
+    this.updateAt = Date.now();
   }
 
-  /*try {
+  try {
     const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
-    selfEvaluation.password = await bcrypt.hash(selfEvaluation.password, salt);
+    this.salt = salt;
+    this.password = await bcrypt.hash(this.password, salt);
   } catch (err) {
     console.log(err);
-  }*/
+  }
 
   next();
-});
+})
+class SelfEvaluationSchema extends Typegoose {
+  @prop({
+    required: true,
+    unique: true
+  })
+  workNumber: string;
 
-SelfEvaluationSchema.methods = {
-  comparePassword: async function (_password) {
+  @prop({required: true})
+  date: Date;
+
+  @prop({required: true})
+  achievement: string;
+
+  @prop({required: true})
+  share: string;
+
+  @prop()
+  contribution: string;
+
+  @prop({ default: Date.now() })
+  createAt: any;
+
+  @prop({ default: Date.now() })
+  updateAt: any;
+
+  @instanceMethod
+  async comparePassword(_password) {
     return await bcrypt
       .compare(_password, this.password)
       .catch(err => console.log(err));
-  },
-};
+  }
+}
 
-SelfEvaluationSchema.statics = {
-  fetch: async function () {
-    return await this
-      .find({})
-      .sort('meta.updateAt');
-  },
-
-  findById: async function (id) {
-    return await this
-      .findOne({_id: id});
-  },
-};
-
-export { SelfEvaluationSchema };
-
+export const SelfEvaluationModel = new SelfEvaluationSchema().getModelForClass(SelfEvaluationSchema);
