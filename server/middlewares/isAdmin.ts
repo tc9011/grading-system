9468508b5bf922db4590cc090dbee7d79d9243b5
10227461 <tang.cheng11@zte.com.cn>
2018-09-03 09:40:32 +0800

    feat(server): add isAdmin middleware

--- server/middlewares/isAdmin.ts
+++ server/middlewares/isAdmin.ts
@@ -1,26 +1,20 @@
 import { Context } from 'koa';
 import * as jsonwebtoken from 'jsonwebtoken';
-import * as Router from 'koa-router';
 
 import { Secret } from '../config/config';
 import { handleError } from '../utils/handle';
 
-const router = new Router();
-export function isAdmin() {
-  return async function isAdmin (ctx: Context, next: any) {
-    console.log(ctx.url);
-    if (ctx.url.includes('passport')) {
-      await next();
-    } else {
-      const token = ctx.request.header.authorization.split(' ')[1];
-      var decoded = jsonwebtoken.verify(token, Secret);
-      console.log(decoded.user.role);
-      if (decoded.user.role < 10) {
-        ctx.status = 401;
-        handleError({ctx, message: '没有权限'});
-        return;
-      }
+export const isAdmin = (ctx: Context, next: any) => {
+  console.log(ctx.url);
+  if (ctx.url.includes('passport')) {
+    next();
+  } else {
+    const token = ctx.request.header.authorization.split(' ')[1];
+    const decoded = jsonwebtoken.verify(token, Secret);
+    console.log(decoded.user.role);
+    if (decoded.user.role < 10) {
+      ctx.status = 401;
+      handleError({ctx, message: '没有权限'});
     }
-  };
-
-}
+  }
+};
