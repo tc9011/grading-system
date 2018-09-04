2346fc14d02ac4d473b1d7a54d0c817aa6bccc11
tc9011 <tc9011@126.com>
2018-09-05 01:37:28 +0800

    feat(modify-password): add layout

--- client/app/views/modify-password/modify-password.component.ts
+++ client/app/views/modify-password/modify-password.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { LoadingService } from '../../core/loading/loading.service';
 
 @Component({
   selector: 'app-modify-password',
@@ -6,10 +9,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./modify-password.component.scss']
 })
 export class ModifyPasswordComponent implements OnInit {
+  form: FormGroup;
+  error = '';
+  visible = false;
+  status = 'pool';
+  progress = 0;
+  passwordProgressMap = {
+    ok: 'success',
+    pass: 'normal',
+    pool: 'exception',
+  };
 
-  constructor() { }
+  constructor(private fb: FormBuilder,
+              public loadingService: LoadingService) {
+    this.form = fb.group({
+      oldPassword: [
+        null,
+        [
+          Validators.required,
+        ]
+      ],
+      password: [
+        null,
+        [
+          Validators.required,
+          Validators.minLength(6),
+        ],
+      ],
+      confirm: [
+        null,
+        [
+          Validators.required,
+          Validators.minLength(6),
+        ],
+      ],
+    });
+  }
+
+  // region: fields
+  get oldPassword() {
+    return this.form.controls.oldPassword;
+  }
+  get password() {
+    return this.form.controls.password;
+  }
+  get confirm() {
+    return this.form.controls.confirm;
+  }
+  // endregion
 
   ngOnInit() {
   }
 
+  submit(): void {
+
+  }
 }
